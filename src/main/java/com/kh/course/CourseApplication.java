3412565a1df518941b0c75f559977fa3e455b6b3
wqnmgdyl <676411234@qq.com>
2021-02-19 09:36:43 +0800

    初始项目

---
+++ src/main/java/com/kh/course/CourseApplication.java
@@ -0,0 +1,13 @@
+package com.kh.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseApplication.class, args);
+    }
+
+}
