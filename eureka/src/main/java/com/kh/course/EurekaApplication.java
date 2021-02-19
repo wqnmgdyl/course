0760efdb0c02625103c3c975824076445e8c5df6
wqnmgdyl <676411234@qq.com>
2021-02-19 09:47:30 +0800

    增加eureka子模块作为注册服务端

--- eureka/src/main/java/com/kh/course/EurekaApplication.java
+++ eureka/src/main/java/com/kh/course/EurekaApplication.java
@@ -3,11 +3,14 @@ package com.kh.course;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author han.ke
+ */
 @SpringBootApplication
-public class CourseApplication {
+public class EurekaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CourseApplication.class, args);
+        SpringApplication.run(EurekaApplication.class, args);
     }
 
 }
