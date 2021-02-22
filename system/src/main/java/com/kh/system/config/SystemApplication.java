de69ebeca008e1f7252a460a0b100e6fc2ba02c2
wqnmgdyl <676411234@qq.com>
2021-02-22 10:09:13 +0800

    配置通用模块server

--- system/src/main/java/com/kh/system/config/SystemApplication.java
+++ system/src/main/java/com/kh/system/config/SystemApplication.java
@@ -1,5 +1,6 @@
-package com.kh.system;
+package com.kh.system.config;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -12,6 +13,7 @@ import org.springframework.core.env.Environment;
  */
 @SpringBootApplication
 @EnableEurekaClient
+@MapperScan("com.kh.server.mapper")
 public class SystemApplication {
     private static final Logger LOG = LoggerFactory.getLogger(SystemApplication.class);
 
