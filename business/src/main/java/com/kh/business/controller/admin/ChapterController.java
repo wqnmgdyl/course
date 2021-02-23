0e8e3fb981f56b533e6961a0fefab575d6dee40b
wqnmgdyl <676411234@qq.com>
2021-02-23 17:58:52 +0800

    前后端数据交互，解决跨域问题

--- business/src/main/java/com/kh/business/controller/admin/ChapterController.java
+++ business/src/main/java/com/kh/business/controller/admin/ChapterController.java
@@ -2,6 +2,7 @@ package com.kh.business.controller.admin;
 
 import com.kh.business.service.ChapterService;
 import com.kh.server.dto.ChapterDto;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,13 +13,14 @@ import java.util.List;
  * @author han.ke
  */
 @RestController
-@RequestMapping("/admin")
+@RequestMapping("/admin/chapter")
+@CrossOrigin
 public class ChapterController {
 
     @Resource
     private ChapterService service;
 
-    @RequestMapping("/chapter")
+    @RequestMapping("/list")
     public List<ChapterDto> list() {
         return service.listChapterDto();
     }
