9c5b586bb42204e9855fc9a29ee3f1c8c40dab69
wqnmgdyl <676411234@qq.com>
2021-02-24 15:48:51 +0800

    简化实体类

--- server/src/main/java/com/kh/server/dto/ResponseDto.java
+++ server/src/main/java/com/kh/server/dto/ResponseDto.java
@@ -1,5 +1,15 @@
 package com.kh.server.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author han.ke
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ResponseDto<T> {
 
     /**
@@ -21,47 +31,4 @@ public class ResponseDto<T> {
      * 返回泛型数据，自定义类型
      */
     private T content;
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public boolean getSuccess() {
-        return success;
-    }
-
-    public void setSuccess(boolean success) {
-        this.success = success;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public T getContent() {
-        return content;
-    }
-
-    public void setContent(T content) {
-        this.content = content;
-    }
-
-    @Override
-    public String toString() {
-        final StringBuffer sb = new StringBuffer("ResponseDto{");
-        sb.append("success=").append(success);
-        sb.append(", code='").append(code).append('\'');
-        sb.append(", message='").append(message).append('\'');
-        sb.append(", content=").append(content);
-        sb.append('}');
-        return sb.toString();
-    }
 }
