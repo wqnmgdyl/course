32780e9e539cceb0aafe6a26bf4b9164588b6ae5
wqnmgdyl <676411234@qq.com>
2021-02-24 13:53:25 +0800

    增加前端分页组件

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -1,11 +1,12 @@
 <template>
     <div>
         <p>
-        <button v-on:click="list()" class="btn btn-white btn-default btn-round">
+        <button v-on:click="list(1)" class="btn btn-white btn-default btn-round">
             <i class="ace-icon fa fa-refresh"></i>
             刷新
         </button>
         </p>
+        <pagination ref="pagination" v-bind:list="list"></pagination>
         <table id="simple-table" class="table  table-bordered table-hover">
             <thead>
             <tr>
@@ -82,7 +83,9 @@
 </template>
 
 <script>
+    import Pagination from "../../components/pagination.vue"
     export default {
+        components: {Pagination},
         name: 'chapter',
         data: function () {
             return {
@@ -91,18 +94,20 @@
         },
         mounted: function () {
             let _this = this;
-            _this.list();
+            _this.$refs.pagination.size = 5;
+            _this.list(1);
         },
         methods: {
-            list() {
+            list(page) {
                 let _this = this;
                 _this.$ajax.post('http://127.0.0.1:9000/business/admin/chapter/list', {
-                    page: 1,
-                    size: 1
+                    page: page,
+                    size: _this.$refs.pagination.size,
                 })
                     .then((response) => {
                         console.log("查询大章列表结果：", response);
                         _this.chapters = response.data.list;
+                        _this.$refs.pagination.render(page,response.data.total)
                     })
             }
         }
--- doc/sql/all.sql
+++ doc/sql/all.sql
@@ -8,3 +8,16 @@ CREATE TABLE chapter  (
 
 insert into chapter (id, course_id, name) values ('00000000','00000000','测试大章一');
 insert into chapter (id, course_id, name) values ('00000001','00000000','测试大章二');
+insert into chapter (id, course_id, name) values ('00000002','00000000','测试大章三');
+insert into chapter (id, course_id, name) values ('00000003','00000000','测试大章四');
+insert into chapter (id, course_id, name) values ('00000004','00000000','测试大章五');
+insert into chapter (id, course_id, name) values ('00000005','00000000','测试大章六');
+insert into chapter (id, course_id, name) values ('00000006','00000000','测试大章七');
+insert into chapter (id, course_id, name) values ('00000007','00000000','测试大章八');
+insert into chapter (id, course_id, name) values ('00000008','00000000','测试大章九');
+insert into chapter (id, course_id, name) values ('00000009','00000000','测试大章十');
+insert into chapter (id, course_id, name) values ('00000010','00000000','测试大章十一');
+insert into chapter (id, course_id, name) values ('00000011','00000000','测试大章十二');
+insert into chapter (id, course_id, name) values ('00000012','00000000','测试大章十三');
+insert into chapter (id, course_id, name) values ('00000013','00000000','测试大章十四');
+insert into chapter (id, course_id, name) values ('00000014','00000000','测试大章十五');
