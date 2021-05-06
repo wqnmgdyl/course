433d4ad841babc9b58e8375d8788e248e7c5c50d
wqnmgdyl <676411234@qq.com>
2021-05-06 15:13:43 +0800

    1.枚举字段，表格显示中文，表单显示下拉框 2.vue中增加过滤器

--- admin/src/views/admin/section.vue
+++ admin/src/views/admin/section.vue
@@ -48,7 +48,7 @@
                     <td>{{section.chapterId}}</td>
                     <td>{{section.video}}</td>
                     <td>{{section.time}}</td>
-                    <td>{{section.charge}}</td>
+                    <td>{{CHARGE | optionKV(section.charge)}}</td>
                     <td>{{section.sort}}</td>
                     <td>{{section.createdAt}}</td>
                     <td>{{section.updatedAt}}</td>
@@ -145,7 +145,9 @@
                                 <div class="form-group">
                                     <label class="col-sm-2 control-label">收费</label>
                                     <div class="col-sm-10">
-                                        <input v-model="section.charge" type="text" class="form-control">
+                                        <select v-model="section.charge" class="form-control">
+                                            <option v-for="o in CHARGE" v-bind:value="o.key">{{o.value}}</option>
+                                        </select>
                                     </div>
                                 </div>
                                 <div class="form-group">
@@ -175,7 +177,8 @@
         data: function () {
             return {
                 section: {},
-                sections: []
+                sections: [],
+                CHARGE: [{key:"C", value:"收费"},{key:"F", value:"免费"}],
             }
         },
         mounted: function () {
